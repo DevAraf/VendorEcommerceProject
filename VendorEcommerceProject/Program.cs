360527faee5a505387c4f8018adbe1b26b6ec4ea
Arafat <devaraf0@gmail.com>
2026-01-03 19:07:01 +0600

    Update Json Converter

--- VendorEcommerceProject/Program.cs
+++ VendorEcommerceProject/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
 
 builder.Services.AddEndpointsApiExplorer();
